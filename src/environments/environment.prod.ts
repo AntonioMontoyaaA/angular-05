be5f35333614bb0a1df7055576d3da4157b4d19d
= <=>
2022-01-17 13:27:43 -0600

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYW50b25pb21vbnRveWFhYSIsImEiOiJja3lkMXRxZ3cwdjF5Mm5ueHM1Nm96dXQ1In0.fdQIEyTrNhH4E47oLMroUQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYW50b25pb21vbnRveWFhYSIsImEiOiJja3lkMXRxZ3cwdjF5Mm5ueHM1Nm96dXQ1In0.fdQIEyTrNhH4E47oLMroUQ'
 };
 
 /*
